a9a1613312f142b6ac3e06f0be762aa89ef19aca
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-11-05 14:47:55 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function(driverName) {
+        return driverName.toLowerCase() === name.toLowerCase()
+    });
+}
+findMatching(drivers, 'Bobby')
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(function(driverLetter) {
+        return driverLetter.toLowerCase().substring(0, letter.length) === letter.toLowerCase()
+    })
+}
+fuzzyMatch(drivers, "Sa")
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driverLookup) {
+        return driverLookup.name === name
+    })
+}
